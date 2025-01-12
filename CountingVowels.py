fdd9dbe0b365f84d107bbf2c936cf14d806be156
admin_dho <davidhorph@hotmail.com>
2025-01-12 13:31:05 -0600

    Added CountVowelsMultipleStrings

--- CountingVowels.py
+++ CountingVowels.py
@@ -1,12 +1,16 @@
 #Count the number of vowels inside a string.
 
-CountVowels = lambda string: {**dict.fromkeys(",".join("aeiouy").split(","), 0), **{i:string.lower().count(i) for i in set(vowel for vowel in string.lower() if (vowel in 'aeiouy'))}, "total": len(list(filter(lambda i: i in "aeiouy", string.lower())))};
+CountVowels = lambda string: {**dict.fromkeys(",".join("aeiouy").split(","), 0), **{i:string.lower().count(i) for i in set(vowel for vowel in string.lower() if (vowel in 'aeiouy'))}, "string": string, "total": len(list(filter(lambda i: i in "aeiouy", string.lower())))};
+
+CountVowelsMultipleStrings = lambda *strings, fn=CountVowels: [fn(i) for i in strings]
 
 print(CountVowels("Hello, world... how are you?"));
 print(CountVowels("The Quick Brown Fox Jumped Over The LAZY ASS DOG!!!"));
 print(CountVowels("HelloO"));
 print(CountVowels("LAAAZYYY"));
 print(CountVowels("Is the sky really falling???"));
+
+print(CountVowelsMultipleStrings("Hello", "EEK", "tHE Quick Brown something or another"))
 
 
 
