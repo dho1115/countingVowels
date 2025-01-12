71b0e199be6d8620d1af67365f8a3fe91176f44f
admin_dho <davidhorph@hotmail.com>
2025-01-12 13:31:45 -0600

    Multiple String w/ comment.

--- CountingVowels.py
+++ CountingVowels.py
@@ -1,7 +1,7 @@
 #Count the number of vowels inside a string.
-
 CountVowels = lambda string: {**dict.fromkeys(",".join("aeiouy").split(","), 0), **{i:string.lower().count(i) for i in set(vowel for vowel in string.lower() if (vowel in 'aeiouy'))}, "string": string, "total": len(list(filter(lambda i: i in "aeiouy", string.lower())))};
 
+#Insert multiple strings.
 CountVowelsMultipleStrings = lambda *strings, fn=CountVowels: [fn(i) for i in strings]
 
 print(CountVowels("Hello, world... how are you?"));
