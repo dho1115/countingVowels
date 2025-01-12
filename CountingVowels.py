73eb94f9da1c4a97067fa1f1d1d8a10785ce3dcb
admin_dho <davidhorph@hotmail.com>
2025-01-12 13:42:39 -0600

    Why I used set( ) instead of list or tuple.

--- CountingVowels.py
+++ CountingVowels.py
@@ -1,5 +1,5 @@
 #Count the number of vowels inside a string.
-CountVowels = lambda string: {**dict.fromkeys(",".join("aeiouy").split(","), 0), **{i:string.lower().count(i) for i in set(vowel for vowel in string.lower() if (vowel in 'aeiouy'))}, "string": string, "total": len(list(filter(lambda i: i in "aeiouy", string.lower())))};
+CountVowels = lambda string: {**dict.fromkeys(",".join("aeiouy").split(","), 0), **{i:string.lower().count(i) for i in set(vowel for vowel in string.lower() if (vowel in 'aeiouy'))}, "string": string, "total": len(list(filter(lambda i: i in "aeiouy", string.lower())))}; #NOTE: I used set() instead of a list or tuple for efficiency purposes.
 
 #Insert multiple strings.
 CountVowelsMultipleStrings = lambda *strings, fn=CountVowels: [fn(i) for i in strings]
